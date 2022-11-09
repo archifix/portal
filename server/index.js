c0d19c64b227d9ab7bb49341d29077fe0cd3e286
archifix <107472693+archifix@users.noreply.github.com>
2022-11-09 17:19:30 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ require('dotenv').config('./.env')
 const express = require('express')
 const sequelize = require('./db.js')
 const modele = require('./models/models')
+const cors = require('cors')
+const router = require('./routes/index')
 
 const PORT = process.env.PORT
 console.log(PORT)
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
 const start = async () => {
     try {
